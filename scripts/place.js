eb7e65d03bf6f3958abf6e3e5f34ce3e510303c0
LeniSoto24 <rsotopedraza17@gmail.com>
2026-05-30 10:15:23 -0600

    w3

---
+++ scripts/place.js
@@ -0,0 +1,8 @@
+const today = new Date();
+const year = today.getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = "Last Modified: " + lastModified;
+
